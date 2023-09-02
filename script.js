c78f4ce379ff99e9da9fe6b9c5b13fd9475815fa
LokeshN1 <115584064+LokeshN1@users.noreply.github.com>
2023-09-02 18:09:07 +0530

    Second Commit

--- script.js
+++ script.js
@@ -2,37 +2,41 @@ let prograss = document.getElementById("progress");
 let song = document.getElementById("song");
 let playBtn = document.getElementById("ctrlIcon2");
 
+
 song.onloadedmetadata = function(){
-    // prograss.max = song.duration;
-    prograss.value = song.currentTime;
+    prograss.max = song.duration;       // progress bar will move until song ends
+    prograss.value = song.currentTime;      // when song will play progress bar's thumb slider will also move
 }
 
+// function created for play / pause the song
 function playPause(){
-    if(ctrlIcon2.classList.contains("fa-pause")){
-        song.pause();
-        ctrlIcon2.classList.add("fa-play");
-        ctrlIcon2.classList.remove("fa-pause");
+    // To pause the song
+    if(ctrlIcon2.classList.contains("fa-pause")){   // it means we have showing pause icon there
+        song.pause();       // stop playing song
+        ctrlIcon2.classList.add("fa-play");        // showing play icon there    
+        ctrlIcon2.classList.remove("fa-pause");      // removing pause icon
 
        
     }
     else{
-        song.play();
-        ctrlIcon2.classList.add("fa-pause");
+    // To play the song
+        song.play();    // start playing song
+        ctrlIcon2.classList.add("fa-pause");        
         ctrlIcon2.classList.remove("fa-play");
     }
 
+    // If song is playing so to move progress bar thumb slider 
     if(song.play){
         setInterval(() => {
             progress.value = song.currentTime;
-        }, 2000)
-        // setTimeout(()=>{
-        //     progress.value = song.currentTime;
-        // },song.duration)
+        }, 500)    // after every 5ms slider will move
+
     }
 
+    // if we move slider so song duration will also affect
     progress.onchange = function(){
         song.play();
-        song.currentTime = prograss.value;
+        song.currentTime = prograss.value;  //song will play from the prograss bar value 
         ctrlIcon2.classList.add("fa-pause");
         ctrlIcon2.classList.remove("fa-play");
     }
